bf067a31eb55f683a8dcebb44dbf49db21d8797e
Goutom Roy <goutom.sust.cse@gmail.com>
2019-10-28 13:52:11 +0600

    Added main() for __main__

--- main/consumer.py
+++ main/consumer.py
@@ -4,15 +4,18 @@ import multiprocessing
 from tasks import live_see_all, parse_profile, parse_posts
 from utils import URL_FACTORY
 
-
 logging.basicConfig(level=logging.INFO)
 
 
-if __name__ == '__main__':
+def main():
     concurrency = multiprocessing.cpu_count()
     w = Worker(faktory=URL_FACTORY, queues=['default', 'busy'], concurrency=1)
     w.register('live_see_all', live_see_all)
     w.register('parse_profile', parse_profile)
     w.register('parse_posts', parse_posts)
     w.run()
+
+
+if __name__ == '__main__':
+    main()
 
--- main/producer.py
+++ main/producer.py
@@ -5,10 +5,15 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-if __name__ == '__main__':
+
+def main():
     my_parser = argparse.ArgumentParser()
     my_parser.add_argument('-nup', '--num_user_to_parse', type=int)
     my_parser.add_argument('-nps', '--num_post_scroll', type=int)
     c_args = my_parser.parse_args()
     with faktory.connection(faktory=URL_FACTORY) as client:
         client.queue('live_see_all', args=[c_args.num_user_to_parse, c_args.num_post_scroll], queue='default')
+
+
+if __name__ == '__main__':
+    main()
